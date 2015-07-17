ca68901cb3da2720c872550b35e22b4dfede78bc
Matt Kaniaris <mkaniaris@gmail.com>
2015-07-16 19:05:38 -0700

    uses ag's binary detection.  thanks ggreer

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,65 @@ use ansi_term::Colour::{Fixed, Green};
 static ALPHABETSIZE : i32 = 256;
 static NEWLINE : u8 = 10;
 
+
+/* This function is very hot. It's called on every file. */
+fn is_binary(buf: &Vec<u8>, buf_len: usize) -> bool {
+  if buf_len == 0 {
+    return true;
+  }
+
+  if buf_len >= 3 && buf[0] == 0xEF && buf[1] == 0xBB && buf[2] == 0xBF {
+    /* UTF-8 BOM. This isn't binary. */
+    return false;
+  }
+
+  // if (buf_len >= 4 && strncmp(buf, "%PDF-", 5) == 0) {
+  //     /* PDF. This is binary. */
+  //     return 1;
+  // }
+  let mut suspicious_bytes: usize = 0;
+  let mut total_bytes: usize = buf.len();
+  if total_bytes > 512 {
+    total_bytes = 512;
+  }
+  let mut i: usize = 0;
+  while i < total_bytes {
+    if buf[i] == 0 {
+      /* NULL char. It's binary */
+      return true;
+    }
+    if (buf[i] < 7 || buf[i] > 14) && (buf[i] < 32 || buf[i] > 127) {
+      /* UTF-8 detection */
+      if buf[i] > 193 && buf[i] < 224 && i + 1 < total_bytes {
+        i += 1;
+        if buf[i] > 127 && buf[i] < 192 {
+          continue;
+        }
+      } else if buf[i] > 223 && buf[i] < 240 && i + 2 < total_bytes {
+        i += 1;
+        if buf[i] > 127 && buf[i] < 192 && buf[i + 1] > 127 && buf[i + 1] < 192 {
+          i += 1;
+          continue;
+        }
+      }
+      suspicious_bytes += 1;
+      /* Disk IO is so slow that it's worthwhile to do this calculation after every suspicious byte. */
+      /* This is true even on a 1.6Ghz Atom with an Intel 320 SSD. */
+      /* Read at least 32 bytes before making a decision */
+      if i >= 32 && (suspicious_bytes * 100) / total_bytes > 10 {
+        return true;
+      }
+    }
+    i += 1;
+  }
+
+  if (suspicious_bytes * 100) / total_bytes > 10 {
+    return true;
+  }
+
+  return false;
+}
+
 fn output(text: &Vec<u8>, position: isize, pattern_length: isize) {
   let mut start: isize = position;
 
@@ -121,9 +180,12 @@ fn handle_path (path: &std::path::Path, query: &String, should_print_file: bool)
   let mut printed_file = false;
   let pattern = query.as_bytes();
   let pattern_length = pattern.len() as isize;
-
+  let buf_len = buf.len();
+  if is_binary(&buf, buf_len) {
+    return;
+  }
   // let newline_cache: Vec<usize> = Vec::new();
-  while i < buf.len() as isize - pattern_length {
+  while i < buf_len as isize - pattern_length {
     j = pattern_length - 1;
     while j >= 0 && pattern[j as usize] == buf[(i+j) as usize] {
       j -= 1;
