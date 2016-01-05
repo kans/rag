c55fc2849ab71246865004c56c32e9babe1b08b6
Matt Kaniaris <mkaniaris@gmail.com>
2016-01-05 15:33:24 -0800

    make use of unsafe pointers in binary file classifier to avoid bounds check on every operation

--- src/utils.rs
+++ src/utils.rs
@@ -2,7 +2,6 @@ static PDF_HEADER: &'static [u8] = &[0x25,0x50,0x44,0x46,0x2d];
 
 /* This function is very hot. It's called on every file. */
 pub fn is_binary(buf: &Vec<u8>, buf_len: usize) -> bool {
-  // TODO: make unsafe!
   if buf_len == 0 {
     return true;
   }
@@ -23,20 +22,24 @@ pub fn is_binary(buf: &Vec<u8>, buf_len: usize) -> bool {
   }
   let mut i: usize = 0;
   while i < total_bytes {
-    if buf[i] == 0 {
+    // avoid bounds checking with normal indexing...
+    let buf_i = unsafe { *buf.get_unchecked(i)};
+    let buf_i_plus_1 = unsafe { *buf.get_unchecked(i+1)};
+
+    if buf_i == 0 {
       /* NULL char. It's binary */
       return true;
     }
-    if (buf[i] < 7 || buf[i] > 14) && (buf[i] < 32 || buf[i] > 127) {
+    if (buf_i < 7 || buf_i > 14) && (buf_i < 32 || buf_i > 127) {
       /* UTF-8 detection */
-      if buf[i] > 193 && buf[i] < 224 && i + 1 < total_bytes {
+      if buf_i > 193 && buf_i < 224 && i + 1 < total_bytes {
         i += 1;
-        if buf[i] > 127 && buf[i] < 192 {
+        if buf_i > 127 && buf_i < 192 {
           continue;
         }
-      } else if buf[i] > 223 && buf[i] < 240 && i + 2 < total_bytes {
+      } else if buf_i > 223 && buf_i < 240 && i + 2 < total_bytes {
         i += 1;
-        if buf[i] > 127 && buf[i] < 192 && buf[i + 1] > 127 && buf[i + 1] < 192 {
+        if buf_i > 127 && buf_i < 192 && buf_i_plus_1 > 127 && buf_i_plus_1 < 192 {
           i += 1;
           continue;
         }
